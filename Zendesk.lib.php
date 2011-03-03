a0df2bcbe53808d21ed8924d169711cdaa5c7ed4
Brian Hartvigsen <brian.andrew@brianandjenny.com>
2011-03-03 02:07:43 +0000

    Fixes issues 6

--- Zendesk.lib.php
+++ Zendesk.lib.php
@@ -9,15 +9,15 @@ Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are
 met:
 
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above
-      copyright notice, this list of conditions and the following
-      disclaimer in the documentation and/or other materials provided
-      with the distribution.
-    * Neither the name of the Zendesk PHP Library nor the names of its
-      contributors may be used to endorse or promote products derived
-      from this software without specific prior written permission.
+	* Redistributions of source code must retain the above copyright
+	  notice, this list of conditions and the following disclaimer.
+	* Redistributions in binary form must reproduce the above
+	  copyright notice, this list of conditions and the following
+	  disclaimer in the documentation and/or other materials provided
+	  with the distribution.
+	* Neither the name of the Zendesk PHP Library nor the names of its
+	  contributors may be used to endorse or promote products derived
+	  from this software without specific prior written permission.
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
@@ -31,7 +31,9 @@ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-CURRENT - 22 January 2011
+SVN - 02 March 2011
+ * Added support for UTF-8 (Issue #6)
+SVN - 22 January 2011
  * Fixed bug with dictionary elements being called arrays
  * Added support for HTTPS (Issue #5)
 v1.02 - 01 March 2010
@@ -135,8 +137,8 @@ class Zendesk
 			if (isset($opts['data']))
 			{
 				$headers = array(
-					'Content-type: application/xml',
-					'Content-Length: ' . strlen($opts['data'])
+					'Content-type: application/xml; charset=utf-8',
+					'Content-Length: ' . mb_strlen($opts['data'])
 				);
 				
 				curl_setopt($this->curl, CURLOPT_POSTFIELDS, $opts['data']);
@@ -164,7 +166,7 @@ class Zendesk
 			if (isset($opts['data']))
 			{
 				$settings['http']['content'] = $opts['data'];
-				$settings['http']['header'] .= "Content-type: application/xml\r\n";
+				$settings['http']['header'] .= "Content-type: application/xml; charset=utf-8\r\n";
 			}
 			
 			if (isset($opts['on-behalf-of']))
